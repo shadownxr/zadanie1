32eba6cb9b5a5555123c7860e108d9b2f9e2caff
Tobiasz <shadownxr1@gmail.com>
2023-01-16 12:03:23 +0100

    Delete console.log

--- resources/js/search_field.ts
+++ resources/js/search_field.ts
@@ -1,8 +1,6 @@
 const input = document.querySelector('#search_input') as HTMLInputElement;
 const input_wrapper = document.querySelector('.search_input_wrapper') as HTMLDivElement;
 
-console.log(input_wrapper);
-
 input.onfocus = () => {
     input_wrapper.classList.add('active');
 }
