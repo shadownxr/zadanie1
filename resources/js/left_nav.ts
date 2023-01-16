04d40de8db5388f96d0d8786b57270eb0ff4ada8
Tobiasz <shadownxr1@gmail.com>
2023-01-16 12:43:42 +0100

    LeftNav rename, nav button fix, left_nav refact

--- resources/js/left_nav.ts
+++ resources/js/left_nav.ts
@@ -1,22 +1,22 @@
 import * as $ from "jquery";
 
-const menu_button = $("#menu_button");
-const menu_background = $(".menu_background");
+const top_nav_button = $("#top_nav_button");
+const left_nav_background = $(".left_nav_background");
 const left_nav = $(".left_nav");
 
 $(() => {
-    menu_button.on("click", () => {
-        menu_background.addClass("active");   
+    top_nav_button.on("click", () => {
+        left_nav_background.addClass("active");   
         left_nav.addClass("active");
     })
 })
 
 $(() => {
     $(document).on("click", (event) => {
-        if($(event.target).closest('#menu_button').length == 0 && left_nav.hasClass("active")){
+        if($(event.target).closest('#top_nav_button').length == 0 && left_nav.hasClass("active")){
             if($(event.target).closest(left_nav).length == 0){
                 left_nav.removeClass("active");
-                menu_background.removeClass("active");
+                left_nav_background.removeClass("active");
             }
         }
     })
