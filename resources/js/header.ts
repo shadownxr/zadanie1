6a4f63035b9e94b0fe3f6a860759ac2ed611c296
Tobiasz <shadownxr1@gmail.com>
2023-01-16 10:52:23 +0100

    Rename menu to top_nav, menu top_nav refactoring

--- resources/js/header.ts
+++ resources/js/header.ts
@@ -2,7 +2,7 @@ import * as $ from 'jquery';
 
 const header_top = document.querySelector('#header_top') as HTMLDivElement;
 const header_anchor = document.querySelector('#header_anchor') as HTMLDivElement;
-const menu = document.querySelector('.menu') as HTMLDivElement;
+const top_nav = document.querySelector('.top_nav') as HTMLDivElement;
 const input_box = document.querySelector('#header_input_box') as HTMLDivElement;
 const search_section = document.querySelector('.search_section') as HTMLDivElement;
 
@@ -10,9 +10,9 @@ $(() => {
     if(window.pageYOffset !== 0){
         header_top.classList.add('top_scrolled');
         header_anchor.classList.add('anchor_scrolled');
-        menu.classList.add('menu_scrolled');
+        top_nav.classList.add('top_nav_scrolled');
 
-        if(($('.search_section').offset().top + $('.search_section').height()) < (window.pageYOffset + menu.offsetHeight)){
+        if(($('.search_section').offset().top + $('.search_section').height()) < (window.pageYOffset + top_nav.offsetHeight)){
             input_box.classList.remove('hidden');
             input_box.classList.add('shown');
             $('.text').addClass('shown');
@@ -23,20 +23,20 @@ $(() => {
         if(window.pageYOffset !== 0){
             header_top.classList.add('top_scrolled');
             header_anchor.classList.add('anchor_scrolled');
-            menu.classList.add('menu_scrolled');
+            top_nav.classList.add('top_nav_scrolled');
         } else {
             header_top.classList.remove('top_scrolled');
             header_anchor.classList.remove('anchor_scrolled');
-            menu.classList.remove('menu_scrolled');
+            top_nav.classList.remove('top_nav_scrolled');
         }
 
-        if(($('.search_section').offset().top + $('.search_section').height()) < (window.pageYOffset + menu.offsetHeight)){
+        if(($('.search_section').offset().top + $('.search_section').height()) < (window.pageYOffset + top_nav.offsetHeight)){
             input_box.classList.remove('hidden');
             input_box.classList.add('shown');
             $('.text').addClass('shown');
         }
 
-        if(($('.search_section').offset().top + $('.search_section').height()) > (window.pageYOffset + menu.offsetHeight)){
+        if(($('.search_section').offset().top + $('.search_section').height()) > (window.pageYOffset + top_nav.offsetHeight)){
             input_box.classList.remove('shown');
             input_box.classList.add('hidden');
             $('.text').removeClass('shown');
